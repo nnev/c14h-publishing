fb1c3579589c976ec5ae47de59c824f9700b216a
Jonas Große Sundrup <cherti@letopolis.de>
2018-04-01 17:48:59 +0200

    Fill datastructure for VOC-compatible XML-generation

--- generate-voc-xml.py
+++ generate-voc-xml.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#cur.execute("SELECT kind, url FROM vortrag_links;") <- link für Material
 
 acronym = "C14H"
 
@@ -48,3 +49,60 @@ all_dates = [e[0] for e in cur.fetchall()]
 
 cur.close()
 conn.close()
+
+"""
+now stich everything together for VOC
+"""
+
+
+out = { "schedule":  OrderedDict([
+        ("version", "1.0"),
+        ("conference",  OrderedDict([
+            ("title", "Chaotische Viertelstunde, NoName e.V."),
+            ("acronym", acronym),
+            ("daysCount", len(all_dates)),
+            ("start", max_date.strftime("%Y-%m-%d")),
+            ("end",   max_date.strftime("%Y-%m-%d")),
+            ("timeslot_duration", "00:15"),
+            ("days", [])
+        ]))
+    ])
+}
+
+for i, d in enumerate(all_dates):
+    out['schedule']['conference']['days'].append(OrderedDict([
+        ('index', i+1),  # day-index must be > 0
+        ('date' , d.strftime("%Y-%m-%d")),
+            ('start', datetime.datetime(d.year, d.month, d.day, 19, 0, 0).isoformat()),
+        ('end', datetime.datetime(d.year, d.month, d.day, 23, 59, 59).isoformat()),
+        ('rooms', OrderedDict())
+    ]))
+
+event_n = OrderedDict([
+    ('id', talkid),
+    ('guid', uuid.uuid4()),
+    # ('logo', None),
+    ('date', date.isoformat()),
+    ('start', '20:00'),
+    ('duration', '0:15'),
+    ('room', 'Chaostreff Heidelberg'),
+    ('slug', '-'.join([acronym, str(talkid), tools.normalise_string(titel)])),
+    ('title', titel),
+    ('subtitle', ''),
+    ('track', ''),
+    ('type', ''),
+    ('language', 'de' ),
+    ('abstract', abstract),
+    ('description', '' ),
+    ('do_not_record', False),
+    ('persons', [ OrderedDict([
+        ('id', 0),
+        ('full_public_name', p.strip()),
+        #('#text', p),
+    ]) for p in speaker.split(',') ]),
+    ('links', ["https://www.noname-ev.de/chaotische_viertelstunde.html#c14h_{}".format(talkid)])
+])
+
+
+with open('schedule-{}-{}.xml'.format(acronym, talkid), 'w') as fp:
+    fp.write(tools.dict_to_schedule_xml(out));
